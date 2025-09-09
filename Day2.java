c21cd9a5668ce1d5f9d2c11d189b72b0e5da938d
Faiz-xe <capwang0123@gmail.com>
2025-09-09 20:39:19 +0800

    Day2.java

--- Day2.java
+++ Day2.java
@@ -1,5 +1,5 @@
 public class Day2 {
-  public static void main(Strong[]args) {
+  public static void main(String[]args) {
     System.out.print("faiz");
     System.out.println("");
     System.out.println("faiz");
