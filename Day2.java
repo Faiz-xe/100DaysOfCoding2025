98e3936a48adc244c0d00342935e8ae8c230f530
Faiz-xe <capwang0123@gmail.com>
2025-09-09 19:39:08 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,12 @@
+public class Day2 {
+  public static void main(Strong[]args) {
+    System.out.print("faiz");
+    System.out.println("");
+    System.out.println("faiz");
+
+    String nama = "faiz";
+    int umur = 19
+
+      System.out.printf("nama saya %s, umur %d tahun", nama, umur);
+  }
+}
