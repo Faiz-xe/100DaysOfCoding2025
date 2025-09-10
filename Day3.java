8a2bdd5f0bd153c7e424e1b3b64ab7d900ff070c
Faiz-xe <capwang0123@gmail.com>
2025-09-10 19:16:59 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,9 @@
+public class Day3 {
+  public static void main(String[]args) {
+    System.out.println("Memahami komentar");
+    //ini adalah komentar satu baris
+  System.out.println("memahami komentar");
+    /*ini komentar multi line
+      */
+  }
+}
