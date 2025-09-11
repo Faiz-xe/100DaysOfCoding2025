c579b0d36ce8da5f500efa29ee5492d2926a1966
Faiz-xe <capwang0123@gmail.com>
2025-09-11 17:56:38 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,11 @@
+public class Day4 {
+  public static void main(String[]args) {
+    System.out.println("Halo\nSelamat datang");
+    // \n membuat baris baru
+  System.out.println("Nama:\tFaiz");
+    System.out.println("Kelas:\tB24");
+    // \t memberi spasi tab
+  System.out.println("Dia berkata:\"Aku mencintaimu\"");
+    // \" menampilkan tanda kutip
+  }
+}
