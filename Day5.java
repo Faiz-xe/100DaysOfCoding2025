0a38f950054cb6e24dd5f685a971daa9bbec6dc9
Faiz-xe <capwang0123@gmail.com>
2025-09-12 19:37:55 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5 {
+    public static void main(String[] args) {
+        
+        // Deklarasi variabel dengan berbagai tipe data bilangan bulat
+        byte angkaByte = 100;
+        short angkaShort = 30000;
+        int angkaInt = 1000000;
+        long angkaLong = 10000000000L; // harus pakai L di akhir
+        
+        // Menampilkan hasil
+        System.out.println("Nilai byte  : " + angkaByte);
+        System.out.println("Nilai short : " + angkaShort);
+        System.out.println("Nilai int   : " + angkaInt);
+        System.out.println("Nilai long  : " + angkaLong);
+    }
+}
