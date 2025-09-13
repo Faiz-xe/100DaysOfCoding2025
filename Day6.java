9a4cd94228cb8f600f596c9216529ae27511333c
Faiz-xe <capwang0123@gmail.com>
2025-09-13 19:27:41 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+public class Day6 {
+    public static void main(String[] args) {
+        // float harus pakai 'f'
+        float angkaFloat = 3.141592f; 
+        // double lebih presisi
+        double angkaDouble = 3.141592653589793;
+
+        System.out.println("Nilai float  : " + angkaFloat);
+        System.out.println("Nilai double : " + angkaDouble);
+    }
+}
