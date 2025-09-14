b9304a3295c3da1cdddb78c7cfadc7c6ecba1e9f
Faiz-xe <capwang0123@gmail.com>
2025-09-14 19:44:58 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Day7 {
+    public static void main(String[] args) {
+        char huruf = 'A';
+        char angka = '9';
+        char simbol = '#';
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Angka: " + angka);
+        System.out.println("Simbol: " + simbol);
+
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Nilai benar: " + benar);
+        System.out.println("Nilai salah: " + salah);
+    }
+}
