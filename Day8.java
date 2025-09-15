8f5b1db20752c0681c20841931ed29473f55dcf0
Faiz-xe <capwang0123@gmail.com>
2025-09-15 15:31:46 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,10 @@
+public class Day8 {
+    public static void main(String[] args) {
+      
+        String nama = "Faiz Asshafwan";
+        String salam = "Halo, selamat datang!";
+        
+        System.out.println(nama);
+        System.out.println(salam);
+    }
+}
