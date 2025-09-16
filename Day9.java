8a13e3c514c670fa1317a0253994c6e2342c0d7c
Faiz-xe <capwang0123@gmail.com>
2025-09-16 20:51:58 +0800

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -2,10 +2,10 @@ public class Day9 {
   public static void main(String[]args) {
     int angka;
     angka = 19;
-    Syatem.out.println("Angka sebelum diupdate : " + angka);
+    System.out.println("Angka sebelum diupdate : " + angka);
 
   angka = 23;
-   Syatem.out.println("Angka setelah diupdate : " + angka);
+   System.out.println("Angka setelah diupdate : " + angka);
   }
 }
  
