946c2e60ecb3103da23c4a5dac1b84e2e1883d1a
Faiz-xe <capwang0123@gmail.com>
2025-09-16 20:36:33 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+public class Day9 {
+  public static void main(String[]args) {
+    int angka;
+    angka = 19;
+    Syatem.out.println("Angka sebelum diupdate : " + angka);
+
+  angka = 23;
+   Syatem.out.println("Angka setelah diupdate : " + angka);
+  }
+}
+ 
