68059927c151a4f444ddbc4b085356fa7b9a1cc6
Faiz-xe <capwang0123@gmail.com>
2025-09-17 19:49:42 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,6 @@
+public class Day10 {
+    public static void main(String[] args) {
+        final int angka = 27; //Nilai variabel tidak dapat diupdate karena merupakan variabel konstanta
+        System.out.println(angka);
+    }
+}
