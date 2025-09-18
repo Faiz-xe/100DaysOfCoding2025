fe843ea553fe4823b65bcf1a61f6d5ef5df89a37
Faiz-xe <capwang0123@gmail.com>
2025-09-18 22:28:15 +0800

    Day11.java

--- Day11.java
+++ Day11.java
@@ -3,7 +3,7 @@ public class Day11 {
     public static void main(String[] args) {
         Scanner faiz = new Scanner(System.in);
         System.out.print("Masukkan Nama Anda : ");
-        String nama = juni.nextLine();
+        String nama = faiz.nextLine();
         System.out.print("Nama Anda adalah " + nama);
     }
 }
