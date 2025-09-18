c1ca4027a5a2a63d7b49f66ba3da10d00dd4e2d9
Faiz-xe <capwang0123@gmail.com>
2025-09-18 21:44:26 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; //Import kelas Scanner
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner faiz = new Scanner(System.in);
+        System.out.print("Masukkan Nama Anda : ");
+        String nama = juni.nextLine();
+        System.out.print("Nama Anda adalah " + nama);
+    }
+}
