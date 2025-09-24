7d9c85d6332c6b345f78ce3de26405e453fb67e5
Faiz-xe <capwang0123@gmail.com>
2025-09-24 20:24:11 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,8 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int a = 27;
+        int b = 6;
+      
+        System.out.println("Hasil sisa bagi : " + a % b); //Hasil 3
+    }
+}
