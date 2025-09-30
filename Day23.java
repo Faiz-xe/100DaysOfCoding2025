3339855dbbfaecc65f29d02770f635a3e329fd66
Faiz-xe <capwang0123@gmail.com>
2025-09-30 19:32:37 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Day23 {
+  public static void main(String[]args) {
+    int panjang = 12;
+    int lebar = 8;
+    int luas = panjang * lebar;
+    System.out.println("luas persegi panjang: " + luas);
+  }
+}
