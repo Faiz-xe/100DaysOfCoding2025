191c7861b7068e85e99aee001750831122a94978
Faiz-xe <capwang0123@gmail.com>
2025-10-02 20:33:08 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,12 @@
+package faiz;
+public class Day25 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.println("nilai i : " + i);
+        }
+        System.out.println("");
+        for (int j = 5; j >= 1; j--) {
+            System.out.println("nilai j : " + j);
+        }
+    }
+}
